0f4bfbaa35e7ec9bdda5bf5f6f8025a46278075c
ТвоеИмя <Email>
2024-12-21 13:01:11 +0300

    чуть переделал парсинг

--- dop_zadachi/4zd/4zd.py
+++ dop_zadachi/4zd/4zd.py
@@ -1,21 +1,32 @@
 import re
 import csv
+import ssl
 import urllib.request
 
 url = "https://msk.spravker.ru/avtoservisy-avtotehcentry/"
-html_content = urllib.request.urlopen(url).read().decode()
+response = urllib.request.urlopen(url)
+html_content = response.read().decode()
 
-pattern = (
-    r'class="org-widget-header__title-link">([^<]+)</a>.*?'
-    r'org-widget-header__meta--location">([^<]+)</span>.*?'
-    r'<dd class="spec__value">([^<]+)</dd>.*?'
-    r'<dd class="spec__value">([^<]+)</dd>'
-)
+with open('html.txt', mode='w', encoding='utf8') as file:
+    file.write(html_content)
 
+#\s* для удаления лишних пробелов
+re_algorithm = r'class="org-widget-header__title-link">\s*([^<]+?)\s*</a>.*?' \
+               r'org-widget-header__meta--location">\s*([^<]+?)\s*</span>.*?' \
+               r'<dt class="spec__index"><span class="spec__index-inner">Телефон</span></dt>.*?' \
+               r'<dd class="spec__value">\s*([^<]+?)\s*</dd>.*?' \
+               r'<dt class="spec__index"><span class="spec__index-inner">Часы работы</span></dt>.*?' \
+               r'<dd class="spec__value">\s*([^<]+?)\s*</dd>'
 
-matches = re.findall(pattern, html_content, re.DOTALL)
+# Поиск совпадений по регулярному выражению
+matches = re.findall(re_algorithm, html_content, re.DOTALL)
 
-with open('output.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile)
+cleaned_matches = [(name.strip(), address.strip(), phone.strip(), hours.strip()) for name, address, phone, hours in matches]
+
+# Запись результатов в CSV-файл с разделителем точка с запятой
+with open('output.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')  # Указываем разделитель ";"
+
     writer.writerow(['Наименование', 'Адрес', 'Телефон', 'Часы работы'])
-    writer.writerows(matches)
+    #строки данных
+    writer.writerows(cleaned_matches)
